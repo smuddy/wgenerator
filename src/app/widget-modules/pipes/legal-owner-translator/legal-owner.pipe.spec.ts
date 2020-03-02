5b746e0db52f57e0c380e1a4a87b4b5466c65196
smuddyx <smuddyx@gmail.com>
2020-03-02 16:28:00 +0100

    Zusätzliche Felder - missing files

---
+++ src/app/widget-modules/pipes/legal-owner-translator/legal-owner.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { LegalOwnerPipe } from './legal-owner.pipe';
+
+describe('LegalOwnerPipe', () => {
+  it('create an instance', () => {
+    const pipe = new LegalOwnerPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
