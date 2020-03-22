32047ec5ef3b41031e5344d21b3fcf63e76befab
smuddyx <smuddyx@gmail.com>
2020-03-22 16:08:17 +0100

    optimizing mobile song text view

--- src/app/widget-modules/components/application-frame/navigation/navigation.component.ts
+++ src/app/widget-modules/components/application-frame/navigation/navigation.component.ts
@@ -1,23 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {faMusic} from '@fortawesome/free-solid-svg-icons/faMusic';
 import {faPersonBooth} from '@fortawesome/free-solid-svg-icons/faPersonBooth';
 import {faUserCog} from '@fortawesome/free-solid-svg-icons/faUserCog';
+import {fromEvent} from 'rxjs';
+import {distinctUntilChanged, map, shareReplay, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.less']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent {
 
   public faSongs = faMusic;
   public faShows = faPersonBooth;
   public faUser = faUserCog;
 
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
+  public readonly windowScroll$ = fromEvent(window, 'scroll').pipe(map(x => window.scrollY), startWith(0), distinctUntilChanged(), shareReplay(1));
 
 }
