baf4e09fc8912a72fe9f3da8283324cdc3b07fca
smuddyx <smuddyx@gmail.com>
2020-05-03 12:40:28 +0200

    delete song

--- src/app/modules/songs/services/song-data.service.ts
+++ src/app/modules/songs/services/song-data.service.ts
@@ -15,6 +15,8 @@ export class SongDataService {
   public list$ = (): Observable<Song[]> => this.dbService.col$(this.collection);
   public read$ = (songId: string): Observable<Song | undefined> => this.dbService.doc$(this.collection + '/' + songId);
   public update$ = async (songId: string, data: Partial<Song>): Promise<void> => await this.dbService.doc(this.collection + '/' + songId).update(data);
-  public add = async (data: Partial<Song>): Promise<string> => (await this.dbService.col(this.collection).add(data)).id
+  public add = async (data: Partial<Song>): Promise<string> => (await this.dbService.col(this.collection).add(data)).id;
+  public delete = async (songId: string): Promise<void> => await this.dbService.doc(this.collection + '/' + songId).delete();
+
 }
 
