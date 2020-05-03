91954fdf153759cd378586b63a3ca0b021abe8f3
smuddyx <smuddyx@gmail.com>
2020-05-03 17:57:33 +0200

    status icons in song list

--- src/app/modules/songs/song-list/list-item/list-item.component.ts
+++ src/app/modules/songs/song-list/list-item/list-item.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Song} from '../../services/song';
 import {faBalanceScaleRight} from '@fortawesome/free-solid-svg-icons/faBalanceScaleRight';
+import {faPencilRuler} from '@fortawesome/free-solid-svg-icons/faPencilRuler';
+import {faCheck} from '@fortawesome/free-solid-svg-icons/faCheck';
 
 @Component({
   selector: 'app-list-item',
@@ -10,6 +12,8 @@ import {faBalanceScaleRight} from '@fortawesome/free-solid-svg-icons/faBalanceSc
 export class ListItemComponent implements OnInit {
   @Input() public song: Song;
   public faLegal = faBalanceScaleRight;
+  public faDraft = faPencilRuler;
+  public faFinal = faCheck;
 
   constructor() {
   }
